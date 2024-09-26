51904f3ffb114460e9e069e59d7cf18cd8d09fa7
Satish <74795240+Satish627@users.noreply.github.com>
2024-09-26 11:45:25 +0200

    logout added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -99,8 +99,21 @@ const loginUser = asyncHandler(async (req, res) => {
         }
     }
 
-)
+)   
+const logout = asyncHandler( async (req, res)=>{
+    res.cookie("token", "", {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(0), //1 day
+        sameSite: "none",
+        // secure: true
+    })
+    return res.status(200).json("User successfully logout")
 
 
-module.exports = { registerUser, loginUser }
+    res.send("User logout")
+})
+
+
+module.exports = { registerUser, loginUser, logout }
 
